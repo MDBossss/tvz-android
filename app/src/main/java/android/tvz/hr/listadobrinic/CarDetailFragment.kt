dbbc0c042c3f75b9f4fe6ae38607a3f2f7e6c0f5
MDBossss <ferrari020203@gmail.com>
2024-05-12 17:49:56 +0200

    added binding

--- app/src/main/java/android/tvz/hr/listadobrinic/CarDetailFragment.kt
+++ app/src/main/java/android/tvz/hr/listadobrinic/CarDetailFragment.kt
@@ -1,5 +1,6 @@
 package android.tvz.hr.listadobrinic
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import android.tvz.hr.listadobrinic.R
+import android.widget.ImageView
 import androidx.fragment.app.ListFragment
+import com.squareup.picasso.Picasso
 
 class CarDetailFragment: ListFragment() {
 
@@ -30,8 +33,20 @@ class CarDetailFragment: ListFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_car_detail, container, false)
+        // if such item exists, fill the values to the view inside that fragment
         if (mItem != null) {
-            (rootView.findViewById<View>(R.id.item_detail) as TextView).text = mItem!!.toString()
+            (rootView.findViewById<View>(R.id.carBrand) as TextView).text = mItem!!.brand
+            (rootView.findViewById<View>(R.id.carModel) as TextView).text = mItem!!.model
+            (rootView.findViewById<View>(R.id.carColor) as TextView).text = mItem!!.color
+
+            Picasso.get().load(mItem!!.imageUrl).into((rootView.findViewById<View>(R.id.carImage) as ImageView))
+
+            (rootView.findViewById<View>(R.id.carImage) as ImageView).setOnClickListener{v: View? -> v?.let {
+                val context = it.context
+                val showImageIntent = Intent(context, ImageActivity::class.java)
+                showImageIntent.putExtra("CAR_DETAILS",mItem)
+                context.startActivity(showImageIntent)
+            }}
         }
 
         return rootView
